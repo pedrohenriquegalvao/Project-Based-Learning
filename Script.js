f18a7e6d3244b3c36305e8f0bac54bc7b5149d46
Pedro <phgs2710@hotmail.com>
2021-11-04 23:20:12 -0300

    Atualizacoes perguntas e inicio localStorage.

--- Script.js
+++ Script.js
@@ -1,3 +1,34 @@
-function newPopup(){
-    varWindow = window.onload ('popup.html', 'popup')
-    }
+let respostasUsuarios = [];
+let usuario = [];
+
+function salvarRespostas() {
+    var nome = document.querySelector('input#nome').value;
+    usuario.push(nome);
+
+    var idade = document.querySelector('input#idade').value;
+    usuario.push(idade);
+
+    var hobby = document.querySelector('input#hobby').value;
+    usuario.push(hobby);
+
+    var formacao = document.querySelector('input#formacao').value;
+    usuario.push(formacao);
+
+    var comunicativa = document.querySelector('input#comunicativa').value;
+    usuario.push(comunicativa);
+
+    var tecnologia = document.querySelector('input#tecnologia').value;
+    usuario.push(tecnologia);
+
+    var cursos = document.querySelector('input#cursos').value;
+    usuario.push(cursos);
+
+    respostasUsuarios.push(usuario);
+
+    localStorage.setItem('respostasUsuarios', respostasUsuarios);
+
+    var containerForum = document.querySelector('div.container-forum');
+    containerForum.innerHTML += nome;
+    
+
+}
