97b5003fbdd74e310bce686d268401079a076983
Leonardo Alves <alvesleo2002@gmail.com>
2021-11-08 17:16:04 -0300

    adição do script da branch-assinatura

---
+++ js/script-assinatura.js
@@ -0,0 +1,49 @@
+var listaCursos = [[0,'correct.png','mensal.png', false], [1,'correct.png', 'trimestral.png', false],
+ [2,'correct.png', 'semestral.png', false], [3,'correct.png', 'anual.png', false]];
+
+var cursosAcessados = []
+
+window.onload = function(){
+
+    montarCardProdutos();
+ 
+}
+
+function montarCardProdutos(){
+
+    document.getElementById("div-pagina").innerHTML = "";
+
+    for(var i = 0; i < listaCursos.length; i++)
+    {
+        var conteudo = "";
+        conteudo += '<div class="div-card">';
+        conteudo += '<div class="div-yellow-line">';
+        conteudo += '</div>'
+        conteudo += '<div class="div-card-img">';
+        conteudo += '<img src="img/'+listaCursos[i][2]+'" />';
+        conteudo += '</div>';
+        conteudo += '<div class="div-card-img-label">';
+        conteudo += '<img src="img/'+listaCursos[i][1]+'" />';
+        conteudo += '<label>Acesso a todos os cursos.</label>'
+        conteudo += '</div>';
+
+
+        if(listaCursos[i][3] == false)
+        {
+            conteudo += '<div class="div-card-acessar" onclick="acessar('+listaCursos[i][0]+')" >';
+            conteudo += 'Matricule-se';
+            conteudo += '</div>';
+        }
+        else
+        {
+            conteudo += '<div class="div-card-acessar curso-acessado" >';
+            conteudo += 'Acessado';
+            conteudo += '</div>';
+        }
+
+        conteudo += '</div>';
+
+        document.getElementById("div-pagina").innerHTML += conteudo;
+    }
+
+}
