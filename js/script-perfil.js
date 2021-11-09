e0d7a15888c58c15dfa04376db572353f2e88fa9
Pedro <phgs2710@hotmail.com>
2021-11-08 23:40:17 -0300

    Merge branch-perfil-script, link entre as paginas e alteracoes no script-perfil.js.

--- js/script-perfil.js
+++ js/script-perfil.js
@@ -1,5 +1,4 @@
 function editar(){
-
     var nomeUsuario = document.querySelector("input#nome-usuario");
     var nome = document.querySelector("input#nome");
     var email = document.querySelector("input#email");
@@ -11,13 +10,16 @@ function editar(){
     senha.disabled = false;
 
     divEditar = document.querySelector("div.divEditar");
-    divEditar.innerHTML += "<button id='buttonSalvar' onclick='salvar()'>Salvar</button>";
+    var buttonSalvar = document.querySelector('button#buttonSalvar');
+    if (buttonSalvar == null) {
+        divEditar.innerHTML += "<button id='buttonSalvar' onclick='salvar()'>Salvar</button>";
+    } 
 
 }
 
 function salvar(){
-    divEditar = document.querySelector("div.divEditar");
-    let buttonSalvar = document.querySelector('button#buttonSalvar');
+    var divEditar = document.querySelector("div.divEditar");
+    var buttonSalvar = document.querySelector('button#buttonSalvar');
     divEditar.removeChild(buttonSalvar);
     var nomeUsuario = document.querySelector("input#nome-usuario");
     var nome = document.querySelector("input#nome");
@@ -27,5 +29,12 @@ function salvar(){
     nome.disabled = true;
     email.disabled = true;
     senha.disabled = true;
+}
 
+function notificar(){
+    alert("Não há nenhuma notificação no momento. Aproveite seu dia! :)")
+}
+
+function cancelarAssinatura(){
+    alert("Você não possui nenhuma assinatura para cancelar no momento.")
 }
